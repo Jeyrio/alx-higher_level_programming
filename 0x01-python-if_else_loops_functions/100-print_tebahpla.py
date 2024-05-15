bd007dc059405c011543d598c027529a2173805a
Jeyrio <j.atueyi@yahoo.com>
2024-05-15 04:37:31 +0000

    done with 100-tebahpla.py file

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 for char in range(ord('z'), ord('a') - 1, -2):
-    print("{}{}".format(chr(char), chr(char - 1 - 32)), end="") 
-    #-1 print in reverse which is from y while 32 convert it to uppercase
+    print("{}{}".format(chr(char), chr(char - 1 - 32)), end="")
+    # -1 print in reverse which is from y while 32 convert it to uppercase
