971109756a69fa139ceae7b3bf7961b9e8345a31
Jeyrio <j.atueyi@yahoo.com>
2024-05-23 20:25:28 +0000

    done with 102-calculation

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,4 +2,4 @@
 
 for char in range(ord('z'), ord('a') - 1, -2):
     print("{}{}".format(chr(char), chr(char - 1 - 32)), end="")
-    # -1 print in reverse which is from y while 32 convert it to uppercase
+    # -1 is substracted from z or 122 to give y while 32 convert it to uppercase
--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
 
-import dis
-
-
 def magic_calculation(a, b, c):
     if a < b:
         return c
     elif c > b:
         return a + b
     else:
-        return (a * b) - c
-
-# print(magic_calculation(1, 2, 3))
-# print(magic_calculation(2, 1, 3))
-# print(magic_calculation(3, 1, 2))
-
-
-dis.dis(magic_calculation)
+        return a * b - c
