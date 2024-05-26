ba9686f604bff92683a2f67033a08511b7b2efd0
Jeyrio <j.atueyi@yahoo.com>
2024-05-26 19:24:04 +0000

    dond with 2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python3
 
-import sys
+from sys import argv
 
-cif
+if __name__ == '__main__':
+    arguments = argv[1:]
+    
+    num_arguments = len(arguments)
+    
+    if num_arguments != 1:
+        plural = 's'
+    else:
+        plural = ''
+    
+    message = "Number of argument{}: {}".format(plural, num_arguments)
+    print(message, end='')
+
+    if num_arguments > 0:
+        print(":")
+        index = 1
+        for arg in arguments:
+            print("{}: {}".format(index, arg))
+            index += 1
+    else:
+        print(".")
