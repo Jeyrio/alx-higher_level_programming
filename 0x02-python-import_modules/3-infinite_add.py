a0b907624d1dc655898b90a9815f07749df7360d
Jeyrio <j.atueyi@yahoo.com>
2024-05-30 14:54:56 +0000

    done with 3-infinite_add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+def main():
+    args = argv[1:]
+    result = 0
+
+    for arg in args:
+        int_arg = int(arg)
+        result += int_arg
+    print(result)
+
+if __name__ == '__main__':
+    main()
