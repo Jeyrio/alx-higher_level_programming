e518f860d68999883cd2ec03b0d83527f114ca53
Jeyrio <j.atueyi@yahoo.com>
2024-06-01 19:51:10 +0000

    done with 2-args.py file

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    
+    from sys import argv, exit
+    from calculator_1 import add, sub, mul, div
+
+    arguments = argv[1:]
+
+    num_argument = len(arguments)
+
+    if num_argument != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(arguments[0])
+    operator = arguments[1]
+    b = int(arguments[2])
+
+    operators = ["+", "-", "*", "/"]
+    
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+
+    if operator != operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,23 +3,19 @@
 from sys import argv
 
 if __name__ == '__main__':
-    arguments = argv[1:]
-    
-    num_arguments = len(arguments)
-    
-    if num_arguments != 1:
-        plural = 's'
-    else:
-        plural = ''
-    
-    message = "Number of argument{}: {}".format(plural, num_arguments)
-    print(message, end='')
 
-    if num_arguments > 0:
-        print(":")
-        index = 1
-        for arg in arguments:
-            print("{}: {}".format(index, arg))
-            index += 1
+    arguments = argv[1:]
+    arg_count = len(arguments)
+
+    if arg_count == 0:
+        print(f"{arg_count} arguments.")
+
+    elif arg_count == 1:
+        print(f"{arg_count} argument:")
+        print(f"{arg_count}: {arguments[0]}")
+
     else:
-        print(".")
+        print(f"{arg_count} arguments:")
+
+        for arg in range(0, arg_count):
+            print(f"{arg + 1}: {arguments[arg]}")
