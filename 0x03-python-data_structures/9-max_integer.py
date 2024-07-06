449c6d4c5f4bc45d30339a3c30b0c02e7491a1d0
Jeyrio <J.atueyi@yahoo.com>
2024-07-07 00:46:56 +0100

    done with 9-max_integer file

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if my_list is None:
+        return None
+    max_value = my_list[0]
+    for elem in my_list:
+        if elem > max_value:
+            max_value = elem
+    return max_value
